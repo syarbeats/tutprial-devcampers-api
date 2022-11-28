14e3e6cac12c3b8be14a4822f20a909b99e816ed
syarbeats <srf.hidayat@gmail.com>
2022-11-28 10:54:24 +0700

    add geocoder address

--- models/bootcamp.js
+++ models/bootcamp.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-// const slugify = require('slugify');
-// const geocoder = require('../utils/geocoder');
+const slugify = require('slugify');
+const geocoder = require('../utils/geocoder');
 
 const BootcampSchema = new mongoose.Schema(
   {
@@ -101,5 +101,31 @@ const BootcampSchema = new mongoose.Schema(
     }
 });
 
+BootcampSchema.pre('save', function(next){
+  this.slug = slugify(this.name, {lower: true});
+  next();
+});
+
+BootcampSchema.pre('save', async function(next){
+
+  //console.log(process.env.GEOCODER_PROVIDER+process.env.GEOCODER_API_KEY);
+
+  const loc =  await geocoder.geocode(this.address);
+  this.location = {
+    type: 'Point',
+    coordinates: [loc[0].longitude, loc[0].latitude],
+    formattedAddress: loc[0].formattedAddress,
+    street: loc[0].streetName,
+    city: loc[0].city,
+    state: loc[0].stateCode,
+    zipcode: loc[0].zipcode,
+    country: loc[0].countryCode,
+  };
+
+  //do not save address to db
+  this.address = undefined;
+
+  next();
+});
 
 module.exports = mongoose.model('Bootcamp', BootcampSchema);

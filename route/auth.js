994d5b944723a2062665301c71ce1181abc9ee5e
syarbeats <srf.hidayat@gmail.com>
2022-12-27 15:24:09 +0700

    update user details & user password

--- route/auth.js
+++ route/auth.js
@@ -5,7 +5,9 @@ const { register,
         login,
         getMe,
         forgotPassword,
-        resetPassword 
+        resetPassword,
+        updateDetails, 
+        updatePassword
         } = require('../controller/auth');
 
 const router = express.Router();
@@ -14,7 +16,9 @@ const router = express.Router();
 router.post('/register', register);
 router.post('/login', login);
 router.get('/me', protect, getMe);
+router.put('/updatedetails', protect, updateDetails);
 router.post('/forgotpassword', forgotPassword);
+router.put('/updatepassword', protect, updatePassword);
 router.post('/resetpassword/:resettoken', resetPassword);
 
 module.exports = router;
